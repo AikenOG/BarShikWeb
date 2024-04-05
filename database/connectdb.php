666cf4bfb2f77d3dd588a4d4f3da031e02e199e9
Aiken <124787991+AikenOG@users.noreply.github.com>
2024-04-05 15:09:52 +0500

    Upload lab project
    Сделана базовая верстка, без админ панели.
    Есть юзер панель

---
+++ database/connectdb.php
@@ -0,0 +1,5 @@
+<!-- database\connectdb.php -->
+
+<?php
+$con = mysqli_connect("localhost", "root", "", "BarShik"); //адресс сервера , логин, пароль, имя бд
+?>
---
+++ header.php
@@ -0,0 +1,18 @@
+<header  content="width=device-width, initial-scale=1">
+     
+        <div class="container">
+        
+            <div class="naw-header">
+            <h1 class="text">БарШик</h1>
+                <img src="../../design\img\Group 8192.png" alt="" class="logo">
+                
+                <div class="naw-menu">
+                    <a href="/">Главная</a>
+                    <a href="">Каталог</a>
+                    <a href="">Корзина</a>
+                    <a href="#footer">Контакты</a>
+                    <a href="user\auth\auth.php">Войти</a>
+                </div>
+            </div>
+        </div>
+    </header>
